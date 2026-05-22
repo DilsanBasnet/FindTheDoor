bb61ba3a190e020d42336d07f2007ef17a933fc0
Dilsan Basnet <officialdilsanacc@gmail.com>
2026-05-22 22:53:04 +0545

    player movement and jumping script added .

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -2,5 +2,49 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-   
+   private float horizontal;
+   private float speed = 8f;
+   private float jump = 15f;
+   private bool facingright = true;
+
+   [SerializeField] private Rigidbody2D rgby;
+   [SerializeField] private LayerMask groundLayer;
+   [SerializeField] private Transform groundCheck;
+
+    void Update() {
+        horizontal= Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetButtonDown("Jump") && IsGrounded()){
+            rgby.linearVelocity = new Vector2(rgby.linearVelocity.x, jump);
+        }
+        if(Input.GetButtonUp("Jump") && rgby.linearVelocity.y > 0f)
+        {
+            rgby.linearVelocity = new Vector2(rgby.linearVelocity.y, rgby.linearVelocity.y * 0.5f) ;
+        }
+        Flip();
+    }
+    private void FixedUpdate()
+    {
+        rgby.linearVelocity = new Vector2(horizontal * speed, rgby.linearVelocity.y);
+    }
+    private void Flip()
+    {
+        if(facingright && horizontal < 0f || !facingright && horizontal > 0f){
+            facingright = !facingright;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale= localScale;
+
+
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+    }
+
+
+
 }
